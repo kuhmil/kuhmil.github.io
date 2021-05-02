b5a3d0abebd407a9acea1ce0cf4868e76e7f0086
kuhmil <kuhmil06@gmail.com>
2021-05-02 08:42:24 -0600

    cleanup

--- js/download.js
+++ js/download.js
@@ -1,23 +1,8 @@
-function dynamic_text() {
-    return "create your dynamic text here";
-}
-
-function download_file(name, contents, mime_type) {
-    mime_type = mime_type || "text/plain";
-
-    var blob = new Blob([contents], {type: mime_type});
-
-    var dlink = document.createElement('a');
-    dlink.download = name;
-    dlink.href = window.URL.createObjectURL(blob);
-    dlink.onclick = function(e) {
-        // revokeObjectURL needs a delay to work properly
-        var that = this;
-        setTimeout(function() {
-            window.URL.revokeObjectURL(that.href);
-        }, 1500);
-    };
-
-    dlink.click();
-    dlink.remove();
+function download_file(fileURL, fileName) {
+  var link = document.createElement('a');
+  link.href = fileURL;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
